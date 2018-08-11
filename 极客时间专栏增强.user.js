d84410f0248b9dd58c1522ecbaed32fbd46c0f06
xyprince <xyprince@users.noreply.github.com>
2018-08-12 00:57:26 +0800

    移除多余代码

--- 极客时间专栏增强.user.js
+++ 极客时间专栏增强.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/xyprince/tampermonkey-script/
 // @updateURL    https://github.com/xyprince/tampermonkey-script/raw/master/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E4%B8%93%E6%A0%8F%E5%A2%9E%E5%BC%BA.user.js
 // @downloadURL  https://github.com/xyprince/tampermonkey-script/raw/master/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E4%B8%93%E6%A0%8F%E5%A2%9E%E5%BC%BA.user.js
-// @version      0.3
+// @version      0.4
 // @description  极客时间 专栏 文章 放大 清除复制限制
 // @author       xyprince
 // @match        https://time.geekbang.org/column/*
@@ -21,28 +21,28 @@
     style.innerHTML = '.article-content > img {display: none}';
     document.head.appendChild(style);
 
-    let observer = new MutationObserver(es => {
-        for(let e of es) {
-            if(e.type == 'childList') {
-                let nodes = e.addedNodes || [];
-                for(let n of nodes) {
-                    let classList = Array.from(n.classList || []);
+    let observer = new MutationObserver(args => {
+        args.filter(a => a.type == 'childList')
+            .map(a => Array.from(a.addedNodes))
+            .forEach(node => {
+                node.filter(n => n.classList)
+                    .forEach(n => {
+                        let classList = Array.from(n.classList);
 
-                    // 放大专栏
-                    if(classList.includes('article')) {
-                        n.style.maxWidth = '90%';
-                    }
+                        // 放大专栏
+                        if(classList.includes('article')) {
+                            n.style.maxWidth = '90%';
+                        }
 
-                    // 清除复制限制
-                    if(classList.includes('main-app')) {
-                        let content = n.querySelector('#article-content');
-                        content.parentNode.replaceChild(content.cloneNode(true), content);
-                        observer.disconnect();
-                    }
-                }
-            }
-        }
+                        // 清除复制限制
+                        if(classList.includes('main-app')) {
+                            let content = n.querySelector('#article-content');
+                            content.parentNode.replaceChild(content.cloneNode(true), content);
+                        }
+                    });
+            });
     });
     let element = document.querySelector('#app');
     observer.observe(element, {attributes: true, childList: true, subtree: true });
+    window.addEventListener('unload', e => observer.disconnect());
 })();
